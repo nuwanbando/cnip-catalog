839fd523fc24830c3b93cd22edfcd5ea3d9b732f
Nuwan Bandara <nuwan@bandara.org>
2019-05-14 16:54:29 -0400

    api defs

--- gateways/wso2-micro-gw/passthrough/build.sh
+++ gateways/wso2-micro-gw/passthrough/build.sh
@@ -4,24 +4,21 @@ wget https://github.com/wso2/product-microgateway/releases/download/v3.0.0-beta2
 
 unzip -q wso2am-micro-gw-toolkit-3.0.0-beta2.zip
 
-echo `pwd`
-
 export MGW_HOME=$(pwd)/wso2am-micro-gw-toolkit-3.0.0-beta2
 
 export PATH=${PATH}:${MGW_HOME}/bin
 
-echo $PATH
-
-micro-gw --help
-
+echo 'initializing the employees project'
 
 micro-gw init employees
+
+echo 'copying API artifacts'
 
 cp employees-api/api_definitions/employees.yaml employees/api_definitions/
 cp employees-api/conf/deployment-config.toml employees/conf/
 
+echo 'building the gateway with employees api'
 micro-gw build employees
 
-tail -n 1000 ${MGW_HOME}/logs/microgw.log
-
+echo 'Displaying local docker images'
 docker images
--- gateways/wso2-micro-gw/passthrough/docker-push.sh
+++ gateways/wso2-micro-gw/passthrough/docker-push.sh
@@ -1,3 +1,4 @@
-#!/bin/bash
+#!/bin/sh
+echo 'Pushing docker image to registry'
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push nuwanbando/employees:v1
