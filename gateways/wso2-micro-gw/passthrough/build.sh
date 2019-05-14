0a4a0adc06f9102c490e1b7b6799ebb21170a3a7
Nuwan Bandara <nuwan@bandara.org>
2019-05-14 15:16:03 -0400

    build script change

--- gateways/wso2-micro-gw/passthrough/build.sh
+++ gateways/wso2-micro-gw/passthrough/build.sh
@@ -4,6 +4,10 @@ wget https://wso2.org/jenkins/view/All%20Builds/job/products/job/product-microga
 
 unzip -q wso2am-micro-gw-toolkit-3.0.0-beta3-SNAPSHOT.zip
 
+echo `pwd`
+
 export PATH=${PATH}:$(pwd)/wso2am-micro-gw-toolkit-3.0.0-beta3-SNAPSHOT/bin
+
+echo $PATH
 
 micro-gw build employees
